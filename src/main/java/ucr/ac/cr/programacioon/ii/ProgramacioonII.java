e69bd6c3a111cf9e4283c734229090a4252572cd
Usuario <Usuario@SPLab1-08>
2024-03-15 08:36:16 -0600

    primera carga

---
+++ src/main/java/ucr/ac/cr/programacioon/ii/ProgramacioonII.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.ac.cr.programacioon.ii;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProgramacioonII {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
